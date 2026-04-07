2bbedd7499a808930303ff61cf204b3217ada9b0
dylanbl <dbl27@proton.me>
2026-04-06 20:14:23 -0500

    modified backend type for torchrun

--- scripts/run_distributed.sh
+++ scripts/run_distributed.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --account=bfep-delta-gpu      # <- This is our ACCESS account 
+#SBATCH --[account_id]      # <- This is our ACCESS account 
 #SBATCH --partition=gpuA40x4          # <- This is the type of node we would like to use 
 #SBATCH --job-name=chexpert_train     # <- The name of our job 
-#SBATCH --nodes=1                     # <- The number of nodes we want 
+#SBATCH --nodes=2                     # <- The number of nodes we want 
 #SBATCH --ntasks-per-node=1           # <- We want to run 1 process per node (PyTorch controls the subprocesses on each node)
 #SBATCH --gpus-per-node=4             # <- How many GPUs we want per node 
 #SBATCH --cpus-per-task=32            # <- How many CPUs we want per node (DataLoader num_workers * gpus_per_node is rule of thumb)
 #SBATCH --mem=64G                     # <- How much RAM per node 
-#SBATCH --time=0:15:00                # <- How long our process can run for 
+#SBATCH --time=0:30:00                # <- How long our process can run for 
 #SBATCH --output=logs/%j.out          # <- Where terminal output goes 
 #SBATCH --error=logs/%j.err           # <- Where error output goes 
 
@@ -17,6 +17,8 @@ module load python
 # Load virtual environment containing required packages 
 source /projects/bfep/lewis1/venv/bin/activate
 
+export NCCL_DEBUG=INFO
+
 # Determine the IP of the control node for SLURM process 
 MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 MASTER_PORT=25827
@@ -25,7 +27,7 @@ MASTER_PORT=25827
 srun torchrun \
     --nproc_per_node=4 \
     --nnodes=$SLURM_NNODES \
-    --node_rank=$SLURM_NODEID \
-    --master_addr=$MASTER_ADDR \
-    --master_port=$MASTER_PORT \
+    --rdzv_id=$SLURM_JOB_ID \
+    --rdzv_backend=c10d \
+    --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
     distributed_main.py --fname config/distributed_config.yaml
